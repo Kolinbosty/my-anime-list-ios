c68fa0c2619b66a420acabfa2c4a8a2edf8140bd
Kolinbosty <qq631159@hotmail.com>
2022-06-12 17:04:10 +0800

    [Feature] Add icons

--- MyAnimeList/MyAnimeList/UIImage+SFSymbol.swift
+++ MyAnimeList/MyAnimeList/UIImage+SFSymbol.swift
@@ -17,6 +17,14 @@ extension UIImage {
         return sfImage(name: "checkmark.circle", size: 24.0)
     }
 
+    static var sf_heart_fill: UIImage? {
+        return sfImage(name: "heart.fill", size: 24.0)?.withTintColor(.red).withRenderingMode(.alwaysOriginal)
+    }
+
+    static var sf_heart: UIImage? {
+        return sfImage(name: "heart", size: 24.0)?.withTintColor(.lightGray).withRenderingMode(.alwaysOriginal)
+    }
+
     private static func sfImage(name: String, size: CGFloat) -> UIImage? {
         let config = UIImage.SymbolConfiguration(font: .systemFont(ofSize: size))
         return UIImage(systemName: name, withConfiguration: config)
